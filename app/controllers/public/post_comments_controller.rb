b3d58260ac76ef818c696ff99d0f2788558128b9
sunny0105yoko <ec2-user@ip-172-31-26-60.ap-northeast-1.compute.internal>
2023-07-12 03:22:14 +0000

    [update]comment_asynchronous

--- app/controllers/public/post_comments_controller.rb
+++ app/controllers/public/post_comments_controller.rb
@@ -1,17 +1,19 @@
 class Public::PostCommentsController < ApplicationController
   
   def create
-    list = List.find(params[:list_id])
-    comment = PostComment.new(post_comment_params)
-    comment.user_id = current_user.id
-    comment.list_id = list.id
-    comment.save
-    redirect_to list_path(list)
+    @list = List.find(params[:list_id])
+    @comment = PostComment.new(post_comment_params)
+    @comment.user_id = current_user.id
+    @comment.list_id = @list.id
+    @comment.save
+    #redirect_to list_path(list)
   end
   
   def destroy
-    PostComment.find(params[:id]).destroy
-    redirect_to list_path(params[:list_id])
+    @list = List.find(params[:list_id])
+    @comment = PostComment.find(params[:id])
+    @comment.destroy
+    #redirect_to list_path(params[:list_id])
   end
 
   private
