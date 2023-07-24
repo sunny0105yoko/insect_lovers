274152b35cd997fce065a8f2b1c1d169366be36b
sunny0105yoko <ec2-user@ip-172-31-26-60.ap-northeast-1.compute.internal>
2023-07-24 13:03:34 +0000

    [update]layout-profiel_image

--- app/controllers/admin/lists_controller.rb
+++ app/controllers/admin/lists_controller.rb
@@ -10,18 +10,18 @@ class Admin::ListsController < ApplicationController
   def edit
     @list = List.find(params[:id])
   end
-  
+
   def destroy
     list = List.find(params[:id])
     if list.destroy
-    redirect_to admin_lists_path, notice: "削除されました"
+    redirect_to admin_user_path(list.user_id), notice: "削除されました"
     end
   end
-  
+
   private
 
   def list_params
     params.require(:list).permit(:name, :image, :introduction, :habitat, :updated_at )
   end
-  
+
 end
--- app/models/admin.rb
+++ app/models/admin.rb
@@ -3,6 +3,5 @@ class Admin < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  #validates :name ,presence: true, length: { minimum: 1, maximum: 20 }
   validates :email ,presence: true, uniqueness: true
 end
