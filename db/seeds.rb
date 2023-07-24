f4321d31ffae530ea9a239beb2a59caf14708d08
sunny0105yoko <ec2-user@ip-172-31-26-60.ap-northeast-1.compute.internal>
2023-07-24 10:24:51 +0000

    [update]seedfile

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,14 +13,75 @@ Admin.create!(
 User.create!(
   [
     {
-      name: 'Test1',
-      email: 'Test1@gmail.com',
+      name: 'アオイ',
+      email: 'Test1@test.com',
       password: '111111',
+      profile_image: ActiveStorage::Blob.create_and_upload!(io: File.open("db/fixtures/man-geed575553_640.jpg"),filename: "man-geed575553_640.jpg")
     },
     {
-      name: 'Test2',
-      email: 'Test2@gmail.com',
+      name: 'アカネ',
+      email: 'Test2@test.com',
       password: '111111',
-    }
+      profile_image: ActiveStorage::Blob.create_and_upload!(io: File.open("db/fixtures/woman-g4b15ed6de_640.jpg"),filename: "woman-g4b15ed6de_640.jpg")
+    },
+    {
+      name: 'バイオリン大好き',
+      email: 'Test3@test.com',
+      password: '111111',
+      profile_image: ActiveStorage::Blob.create_and_upload!(io: File.open("db/fixtures/adventure-g666090654_640.jpg"),filename: "adventure-g666090654_640.jpg")
+    },
+  ]
+ )
+
+List.create!(
+  [
+    {
+      name: '蝶',
+      introduction:
+        %Q{ベランダにやってきたカバマダラの仲間かなと思います。\n},
+      habitat: "東京都荒川区",
+      user_id: 1,
+      image: ActiveStorage::Blob.create_and_upload!(io: File.open("db/fixtures/butterfly-354528_640.jpg"),filename: "butterfly-354528_640.jpg")
+    },
+    {
+      name: '美しい蝶',
+      introduction:
+        %Q{花とのコントラストが美しかったので思わず、写真を撮りました。\n蝶の名前は不明},
+      habitat: "東京都練馬区",
+      user_id: 1,
+      image: ActiveStorage::Blob.create_and_upload!(io: File.open("db/fixtures/butterfly-gba394e264_640.jpg"),filename: "butterfly-gba394e264_640.jpg")
+    },
+    {
+      name: '美しいトンボ',
+      introduction:
+        %Q{珍しいトンボをみかけました。\n蝶の名前は不明},
+      habitat: "東京都練馬区",
+      user_id: 2,
+      image: ActiveStorage::Blob.create_and_upload!(io: File.open("db/fixtures/dragonfly-g2d002fa95_640.jpg"),filename: "dragonfly-g2d002fa95_640.jpg")
+    },
+    {
+      name: '美しいトンボ２',
+      introduction:
+        %Q{珍しいトンボをみかけました。\nこのトンボの名前など詳しい方、どなたかご存知の方、ご教示ください。},
+      habitat: "東京都",
+      user_id: 2,
+      image: ActiveStorage::Blob.create_and_upload!(io: File.open("db/fixtures/dragon-fly-g86e17fb4b_640.jpg"),filename: "dragon-fly-g86e17fb4b_640.jpg")
+    },
+    {
+      name: 'ミヤマクワガタ',
+      introduction:
+        %Q{おそらくミヤマクワガタと思われます。},
+      habitat: "東京都文京区",
+      user_id: 3,
+      image: ActiveStorage::Blob.create_and_upload!(io: File.open("db/fixtures/stag-beetle-g40e62933c_640.jpg"),filename: "stag-beetle-g40e62933c_640.jpg")
+    },
+    {
+      name: '蜂',
+      introduction:
+        %Q{ミツバチでしょうか。お花見中に見つけました。},
+      habitat: "福岡市博多区",
+      user_id: 3,
+      image: ActiveStorage::Blob.create_and_upload!(io: File.open("db/fixtures/bee-g631ef3f63_640.jpg"),filename: "bee-g631ef3f63_640.jpg")
+    },
   ]
 )
